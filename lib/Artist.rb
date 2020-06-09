f3697bcf65d65e243387099f1e7b4244cd68aba9
Evan Burton <evan@heyevan.com>
2020-06-09 23:24:48 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,5 +1,5 @@
 class Artist
-attr_accessor :name, :songs 
+attr_accessor :name, :songs
 
   def initialize
   save
@@ -16,6 +16,7 @@ attr_accessor :name, :songs
   def self.reset_all
   @@all.clear
   end
+
 
 
 
