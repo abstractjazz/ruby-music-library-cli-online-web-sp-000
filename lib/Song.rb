061c48c7bed18a07b9800d98948a9cd9fcfe6f63
Evan Burton <evan@heyevan.com>
2020-06-10 00:32:31 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -1,38 +1,57 @@
-class Song
-attr_accessor :name, :songs
+class Song < Super
 
-extend Concerns::Findable
+  attr_reader :artist, :genre
 
-@@all = [ ]
-
-  def initialize(name, artist, genre)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
-    @genre = genre
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
+  end
+
+  def self.create(name)
+    instance = Song.new(name)
+    instance.save
+    instance
+  end
+
+  def artist=(artist)
     @artist = artist
-    @@all << self
+    self.artist.add_song(self)
   end
 
-
-  def save
-  @@all << self
+  def genre=(genre)
+    @genre = genre
+    self.genre.add_song(self)
   end
 
-  def self.all
-  @@all
+  def self.new_from_filename(file_name)
+    file_bits = file_name.gsub(/(\.mp3)/,'')
+    file_bits = file_bits.split(" - ")
+    artist = Artist.find_or_create_by_name(file_bits[0])
+    genre = Genre.find_or_create_by_name(file_bits[2])
+    song = Song.find_or_create_by_name(file_bits[1])
+    song.genre = genre
+    song.artist = artist
+    song
   end
 
-  def self.destroy_all
-  @@all.clear
+  def self.create_from_filename(file_name)
+    file = Song.new_from_filename(file_name)
+    file.save
+    file
   end
 
-  def save
-  @@all << self
+  def self.find_by_name(song_name)
+    self.all.find {|song|song.name == song_name}
   end
 
-  def self.create(song)
-  song = self.new(song)
-  song.save
-  song
+  def self.find_or_create_by_name(song_name)
+    song = self.find_by_name(song_name)
+    if song == nil
+      self.create(song_name)
+    else
+      song
+    end
   end
 
 end
